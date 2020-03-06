331359e01ba16ec4599f5e76f93dbc55093b618c
Asif <mhdaxif@gmail.com>
2020-03-06 22:12:05 +0500

    Add RoleClaims

---
+++ TahirMvc123/Models/RoleClaim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TahirMvc123.Models
+{
+    public class RoleClaim : Entity
+    {
+        public string Value { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
